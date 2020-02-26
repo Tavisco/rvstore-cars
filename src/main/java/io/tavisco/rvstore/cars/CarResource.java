a20324b2a4fef00e9c8f54e35a68b16a46f6471a
Tavisco <Tavisco@users.noreply.github.com>
2020-02-26 19:24:12 -0300

    feat(*): the user now has to authenticate to add new cars

--- src/main/java/io/tavisco/rvstore/cars/CarResource.java
+++ src/main/java/io/tavisco/rvstore/cars/CarResource.java
@@ -1,19 +1,29 @@
 package io.tavisco.rvstore.cars;
 
 import java.net.URI;
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
@@ -28,21 +38,45 @@ import lombok.extern.slf4j.Slf4j;
 @Path("/cars")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@RequestScoped
 public class CarResource {
 
+    @Inject  
+    JsonWebToken jwt;
+
+    @Inject  
+    @Claim("groups")  
+    private Set<String> groups; 
+
     @GET
+    @Path("/auth")
+    @RolesAllowed({"Everyone"})
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloRolesAllowed(@Context SecurityContext ctx) {  
+        Principal caller =  ctx.getUserPrincipal();  
+        String name = caller == null ? "anonymous" : caller.getName();  
+        boolean hasJWT = jwt != null;  
+        String groupsString = groups != null ? groups.toString() : "";  
+        String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s, groups: %s\"", name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJWT, groupsString);  
+        return helloReply;  
+    }
+
+    @GET
+    @PermitAll
     public List<Car> getAllItems() {
         log.finest("Getting all items...");
         return Car.listAll(Sort.by("name"));
     }
 
     @GET
+    @PermitAll
     @Path("/search/{name}")
     public List<Car> findByName(@PathParam String name) {
         return Car.findByName(name);
     }
 
     @GET
+    @PermitAll
     @Path("{id}")
     public Car findById(@PathParam Long id) {
         Car car = Car.findById(id);
@@ -54,6 +88,7 @@ public class CarResource {
 
     @POST
     @Transactional
+    @RolesAllowed({"Everyone"})
     public Response newCar(@Context UriInfo uriInfo, Car newCar) {
         if (newCar.id != null) {
             throw new WebApplicationException("Id was invalidly set on request.", Status.NOT_ACCEPTABLE);
